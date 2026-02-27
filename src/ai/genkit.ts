9d5b68e0e3d404293935c083ed59f6a79654882c
Waleed Ramdan (waleed ramdan) <mediabogy@gmail.com>
2026-02-27 04:33:36 +0000

    المشكلة لسه موجوده

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
-import * as genkitCore from '@genkit-ai/core';
+import { genkit } from '@genkit-ai/core';
 import {googleAI} from '@genkit-ai/google-genai';
 
-export const ai = genkitCore.genkit({
+export const ai = genkit({
   plugins: [googleAI()],
   model: 'googleai/gemini-1.5-pro-latest',
 });
